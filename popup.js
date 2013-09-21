87cac9fc2861b8e7f4024e882dfc9a2abe86599e
Peter Xiao <pterxiao@gmail.com>
2013-09-21 15:17:46 -0400

    Added shit

--- popup.js
+++ popup.js
@@ -1,17 +1,25 @@
+var template = [
+  '<p><img width="19px" height="19px" src="{{faviconUrl}}"/>',
+  ' {{value}}</p>'
+].join('');
 chrome.tabs.query({}, function(tabs) {
   var $tabsearch = $('#tab-search');
   titles = tabs.map(function(tab) {
+    titleTokens = tab.title.split(' ');
     return {
       value: tab.title,
-      tokens: tab.title.split(' '),
+      tokens: titleTokens,
+      faviconUrl: tab.favIconUrl,
       id: tab.id
-    }
+    };
   });
   $tabsearch.on('typeahead:selected', function(_, datum) {
     chrome.tabs.update(datum.id, {active: true});
   });
   $tabsearch.typeahead({
     name: 'tabs',
-    local: titles
+    local: titles,
+    template: template,
+    engine: Hogan
   });
 });
