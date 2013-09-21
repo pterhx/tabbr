cadbe5ce769fa9b182dde334213ed5c94e02527e
Donald Huang <don.hcd@gmail.com>
2013-09-21 15:08:01 -0400

    oops I was wrong before

--- popup.js
+++ popup.js
@@ -1,9 +1,13 @@
 chrome.tabs.query({}, function(tabs) {
   var $tabsearch = $('#tab-search');
   titles = tabs.map(function(tab) {
-    return tab.title;
+    return {
+      value: tab.title,
+      tokens: tab.title.split(' '),
+      id: tab.id
+    }
   });
-  $tabsearch.on('typeahead:selected', function(datum) {
+  $tabsearch.on('typeahead:selected', function(_, datum) {
     chrome.tabs.update(datum.id, {active: true});
   });
   $tabsearch.typeahead({
